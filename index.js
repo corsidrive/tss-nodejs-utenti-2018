5025e80cd1b9aa3931b0b2a8dce1e5b8f9790c8c
Roberto Sarasso <sarassoroberto@gmail.com>
2018-04-18 22:57:24 +0200

    aggiunto mngo js

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.json({a:"b"}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+ 
+
+  ///mongodb://<dbuser>:<dbpassword>@ds247499.mlab.com:47499/tss-formarete
